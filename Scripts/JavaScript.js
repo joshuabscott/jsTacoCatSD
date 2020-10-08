6254f7dc073304a881ae63a9c0cbbccd6d60f5f3
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2020-10-07 22:54:50 -0700

    new JS
    update to js file

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,20 @@
+﻿//4. Write a program that determines whether a word is a Palindrome
+function Palindrome() {
+	var text = document.getElementById("Palindrome").innerHTML;
+	document.getElementById("Palindrome").innerHTML = function ();
+	//step 1
+	let inputWord = document.getElementById("reverseString").value;
+
+	//step 2
+	// loop
+	let reverseWord = ""
+	for (let i = inputWord.length - 1; i >= 0; i++)
+		reverseWord += inputWord.charAt[i];
+
+
+	//step 3
+	// answer
+	if (inputWord == reverseWord)
+		alert{ }
+	document.getElementById("reverseOutput").innerHTML = reverseWord;
+}
